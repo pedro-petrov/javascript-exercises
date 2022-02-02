a74fbf31ad986e75af02922bb7d4627012477305
pedro-petrov <petrovonrails@gmail.com>
2022-02-02 19:28:50 +0000

    commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,40 +1,49 @@
 const findTheOldest = function(people) {
-    oldestPerson = "";
-    oldestAge = 0;
-    ageCheck = 0;
-    for (person of people) {
-        ageCheck = person.yearOfDeath - person.yearOfBirth;
-        if (ageCheck > oldestAge) {
-            oldestAge = ageCheck;
-            //console.log("The oldest age is: " + oldestAge);
-            oldestPerson = person.name;
-            //console.log("The current oldest person is: " + oldestPerson);
-        }
+  var holdName = "";
+  var holdAge = 0;
+  var currentYear = new Date().getFullYear();
+  
+  for (person of people) {
+    if (person.yearOfDeath) {
+      var personAge = person.yearOfDeath - person.yearOfBirth;
+      if (personAge > holdAge) {
+        holdAge = personAge;
+        holdName = person.name;
+        console.log("1st if: " + holdName);
+      }
+    } else {
+      var personAge = currentYear - person.yearOfBirth;
+      if (personAge > holdAge) {
+        holdAge = personAge;
+        holdName = person.name;
+        console.log("2nd if: " + holdName);
+      }
     }
-return oldestPerson;
+  }
+  var oldest = people.find(item => item.name = holdName);
+  return oldest;
 };
 
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
+/* const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
 
 test = findTheOldest(people);
-console.log(test);
-
+console.log(test); */
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,29 @@
-const caesar = function() {
+const caesar = function(str, num) {
+    strArray = [...str];
+    strArrayUcode = [];
+    for (letter of strArray) {
+        // This will be the unicode value with the shift factor added
+        shiftCode = (letter.charCodeAt(0) + num);
+        // Check if it's UPPER
+        if (shiftCode > 65 && shiftCode <= 90) {
+            strArrayUcode.push(shiftCode);
+        }
+        // Check if it's LOWER
+        if (shiftCode > 97 && shiftCode <= 122) {
+            strArrayUcode.push(shiftCode);
+        }
+    }
 
-};
+    console.log(strArray);
+    console.log(strArrayUcode);
+}
+test = caesar('Hey', 1);
+console.log(test);
+
+/* NOTES
+a...z >>> 97...122
+A...Z >>> 65...90
+*/
 
 // Do not edit below this line
 module.exports = caesar;
